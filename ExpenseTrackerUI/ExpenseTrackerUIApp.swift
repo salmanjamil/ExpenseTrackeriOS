e99ed1df50d3d0c1850f7c1590041ebd1b719141
salman jamil <salmanjamil.140@gmail.com>
2022-04-16 11:59:08 +0500

    Initial Commit

---
+++ ExpenseTrackerUI/ExpenseTrackerUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerUIApp.swift
+//  ExpenseTrackerUI
+//
+//  Created by Salman Jamil on 16/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
